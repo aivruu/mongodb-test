1df8862d3fc062ae3c73d67fa7601e18a92d7cb7
InitSync <benjaalejandro206@gmail.com>
2023-01-17 16:03:36 -0300

    feat(DatabaseManager): add interface for manage the database connection and properties

---
+++ src/main/java/me/initsync/mongodb/test/api/model/database/DatabaseManager.java
@@ -0,0 +1,41 @@
+package me.initsync.mongodb.test.api.model.database;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+public interface DatabaseManager {
+	/**
+	 * Establish the database connection.
+	 *
+	 * @return A boolean value. True if the connection was established successful, else return false.
+	 */
+	boolean establishConnection();
+	
+	/**
+	 * Sets the database booting properties.
+	 */
+	void setDatabaseProperties();
+	
+	/**
+	 * Returns the mongo client.
+	 *
+	 * @return The client.
+	 */
+	MongoClient getClient();
+	
+	/**
+	 * Returns the database object.
+	 *
+	 * @return A MongoDatabase object.
+	 */
+	MongoDatabase getDatabase();
+	
+	/**
+	 * Returns the database collection (table) "Data".
+	 *
+	 * @return The "Data" collection.
+	 */
+	MongoCollection<Document> getDataCollection();
+}
