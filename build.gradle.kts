82d34be765bc6c2aabdfbb78c68ec42c6e9ba979
InitSync <benjaalejandro206@gmail.com>
2023-01-17 13:24:26 -0300

    feat: add main project files

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+	id("com.github.johnrengelman.shadow") version("7.1.2")
+	id("net.minecrell.plugin-yml.bukkit") version("0.5.2")
+	`java-library`
+}
+
+val directory = property("group") as String
+val release = property("version") as String
+
+repositories {
+	mavenLocal()
+	maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+	mavenCentral()
+}
+
+dependencies {
+	compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
+	compileOnly("org.mongodb:mongo-java-driver:3.12.7")
+}
+
+bukkit {
+	name = "MongoDB-Test"
+	main = "$directory.MongoDBTest"
+	authors = listOf("InitSync")
+	
+	apiVersion = "1.13"
+	version = release
+	
+	commands {
+		register("test") {
+			description = "-> Command to manage the player stats."
+			aliases = listOf("playerstats")
+		}
+	}
+}
+
+tasks {
+	shadowJar {
+		archiveFileName.set("MongoDB-Test-$release.jar")
+		destinationDirectory.set(file("$rootDir/bin/"))
+	}
+	
+	clean {
+		delete("$rootDir/bin/")
+	}
+}
